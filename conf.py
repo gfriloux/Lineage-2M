5e2e8d55b128cf45a393e68e4b399231abc58186
Guillaume Friloux <guillaume@friloux.me>
2022-06-07 19:41:28 +0200

    Add conf.py
    Adding base config file for ReadTheDocs

---
+++ conf.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import time
+
+import sphinx_rtd_theme
+import time
+
+html_logo = "Images/logo.png"
+#html_favicon = "images/favicon.ico"
+project = u'Lineage 2M'
+
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = ['_build', 'html', 'doctrees']
+
+locale_dirs = ['locale/']
+gettext_compact = False
+language = "en"
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+extensions = [
+]
